7725fa46ac7a58e0b26d02ab6436ce2ba3af8ae8
vladimir <vladistar200302@gmail.com>
2022-07-03 16:26:24 -0500

    feat: finished aside component

---
+++ src/components/molecules/aside/Aside.tsx
@@ -0,0 +1,22 @@
+import "./styles.css";
+import logo from "../../../assets/svgs/logo.svg";
+import links from "../../../utils/router";
+import TextIcon from "../../atoms/text-icon/TextIcon";
+import UserBubble from "../../atoms/user-bubble/UserBubble";
+import vladimir from "../../../assets/imgs/vladimir.jpg";
+
+export default function Aside() {
+  return (
+    <aside className="aside">
+      <img alt="logo-muta" src={logo} />
+      <ul>
+        {links.map((link) => (
+          <li key={link.label}>
+            <TextIcon icon={link.icon} text={link.label} />
+          </li>
+        ))}
+      </ul>
+      <UserBubble img={vladimir} text="Vladimir Castaneda" />
+    </aside>
+  );
+}
