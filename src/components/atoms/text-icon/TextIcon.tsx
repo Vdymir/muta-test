d2035feccdccbf1b0f07c20b84c3bd5a6916cd8d
vladimir <vladistar200302@gmail.com>
2022-07-03 17:50:08 -0500

    feat: add new prop (fullWidth)

--- src/components/atoms/text-icon/TextIcon.tsx
+++ src/components/atoms/text-icon/TextIcon.tsx
@@ -4,17 +4,22 @@ interface Props {
   text: string;
   icon: any;
   iconRight?: boolean;
+  fullWidth?: boolean;
 }
 
 export default function TextIcon({
   text,
   icon: Component,
   iconRight = false,
+  fullWidth = false,
 }: Props) {
   return (
     <div
       className="textIcon"
-      style={{ flexDirection: iconRight ? "row" : "row-reverse" }}
+      style={{
+        flexDirection: iconRight ? "row" : "row-reverse",
+        width: fullWidth ? "100%" : "auto",
+      }}
     >
       <p>{text}</p>
       <Component />
