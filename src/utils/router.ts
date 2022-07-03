84ce7639696d079556eb6f3406c4fe55aa9632f9
vladimir <vladistar200302@gmail.com>
2022-07-03 16:25:16 -0500

    chore: util router

---
+++ src/utils/router.ts
@@ -0,0 +1,30 @@
+import Help from "../assets/svgs/components/Help";
+import Sliders from "../assets/svgs/components/Sliders";
+import Trash from "../assets/svgs/components/Trash";
+import Truck from "../assets/svgs/components/Truck";
+import Users from "../assets/svgs/components/Users";
+
+const links = [
+  {
+    label: "Usuarios",
+    icon: Users,
+  },
+  {
+    label: "Recolecciones",
+    icon: Trash,
+  },
+  {
+    label: "Entradas ACU",
+    icon: Truck,
+  },
+  {
+    label: "Configurar",
+    icon: Sliders,
+  },
+  {
+    label: "Ayuda",
+    icon: Help,
+  },
+];
+
+export default links;
