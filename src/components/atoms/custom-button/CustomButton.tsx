9fa71bda090876d9f515deb7bb01b34b16985485
vladimir <vladistar200302@gmail.com>
2022-07-04 12:10:42 -0500

    feat: new props fullWidth

--- src/components/atoms/custom-button/CustomButton.tsx
+++ src/components/atoms/custom-button/CustomButton.tsx
@@ -6,6 +6,7 @@ interface Props {
   icon?: any;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
   disabled?: boolean;
+  fullWidth?: boolean;
 }
 
 export default function CustomButton({
@@ -13,10 +14,15 @@ export default function CustomButton({
   icon: Component = null,
   onClick,
   disabled = false,
+  fullWidth = false,
 }: Props) {
   return (
     <div className="customButton">
-      <button onClick={onClick} disabled={disabled}>
+      <button
+        onClick={onClick}
+        disabled={disabled}
+        style={{ width: fullWidth ? "100%" : ''}}
+      >
         <p>{text}</p>
         {Component && <Component />}
       </button>
